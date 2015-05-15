207b80f97c6173a7395c6500033edd79b3b71831
root <fxtrdnsk@gmail.com>
2015-05-14 21:32:12 -0700

    test

--- problems/test.py
+++ problems/test.py
@@ -1,1 +1,16 @@
-input('asd')
+l = range(1, 11)
+print l
+for i in (0, len(l)):
+    l[i] = l[i]*2
+    print l
+'''
+l = l.reverse()
+l = l.remove(l[4])
+l = l.index(3)
+l = l.sort(l)
+l = l.append("Alex")
+print l
+'''
+whilw 4 in l:
+l.remove(4)
+l.pop(3)
