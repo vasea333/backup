a477f87fb81edb9c935849f9ff090875408e52e5
root <fxtrdnsk@gmail.com>
2015-06-07 00:34:19 -0700

    12

--- api_framework/src/pubapiutils.py
+++ api_framework/src/pubapiutils.py
@@ -1,85 +1,234 @@
 import requests
 import json
+from ConfigParser import SafeConfigParser
+import os
+import inspect
+from random import randint
 
 
-def create_folder(folder_name, path=None, domain=None, method=None, content_type=None, accept=None,
-                  username=None, password=None):
-    if domain is None:
-        domain = 'https://istepanko20.qa-egnyte.com'
-    if method is None:
-        method = 'POST'
-    if content_type is None:
-        content_type = 'application/json'
-    if accept is None:
-        accept = 'application/json'
-    if username is None:
-        username = 'admin'
-    if password is None:
-        password = '12345678'
-    if path is None:
-        path = '/Shared/test_folder/'
-
-    endpoint = '/public-api/v1/fs'
-    url = '%s%s%s%s' % (domain, endpoint, path, folder_name)
-    headers = dict()
-    headers['Content-Type'] = content_type
-    headers['Accept'] = accept
-    data = dict()
-    data['action'] = 'add_folder'
-    data = json.dumps(data)
-
-    r = requests.request(
-        method=method,
-        url=url,
-        headers=headers,
-        data=data,
-        auth=(username, password)
-    )
-
-    try:
-        json_resp = json.loads(r.content)
-    except ValueError:
-        json_resp = 'NoJSON'
-
-    r.json = json_resp
-    return r
+class Config:
+    def __init__(self):
+        self.parser = SafeConfigParser()
+        if os.path.isfile('config.ini'):
+            self.parser.read('config.ini')
+        else:
+            print('No config.ini found under root folder.')
+        self.domain = self.parser.get('Server', 'url')
+        self.admin_login = self.parser.get('Server', 'user')
+        self.password = self.parser.get('Server', 'passwd')
+        self.puser = self.parser.get('Server', 'puser')
+        self.testpath = self.parser.get('Server', 'testpath')
 
 
-def del_folder(folder_name, path=None, domain=None, method=None, content_type=None, accept=None,
-                  username=None, password=None):
-    if domain is None:
-        domain = 'https://istepanko20.qa-egnyte.com'
-    if method is None:
-        method = 'DELETE'
-    if content_type is None:
-        content_type = 'application/json'
-    if accept is None:
-        accept = 'application/json'
-    if username is None:
-        username = 'admin'
-    if password is None:
-        password = '12345678'
-    if path is None:
-        path = '/Shared/test_folder/'
+class Calls:
+    def __init__(self):
+        self.config = Config()
 
-    endpoint = '/public-api/v1/fs'
-    url = '%s%s%s%s' % (domain, endpoint, path, folder_name)
-    headers = dict()
-    headers['Content-Type'] = content_type
-    headers['Accept'] = accept
+    def create_folder(self, folder_name, path=None, domain=None, method=None, content_type=None, accept=None,
+                      username=None, password=None, print_call=True):
+        if domain is None:
+            domain = self.config.domain
+        if method is None:
+            method = 'POST'
+        if content_type is None:
+            content_type = 'application/json'
+        if accept is None:
+            accept = 'application/json'
+        if username is None:
+            username = self.config.admin_login
+        if password is None:
+            password = self.config.password
+        if path is None:
+            path = self.config.testpath
+
+        endpoint = '/public-api/v1/fs'
+        url = '%s%s%s/%s' % (domain, endpoint, path, folder_name)
+        headers = dict()
+        headers['Content-Type'] = content_type
+        headers['Accept'] = accept
+        data = dict()
+        data['action'] = 'add_folder'
+        data = json.dumps(data)
+
+        r = requests.request(
+            method=method,
+            url=url,
+            headers=headers,
+            data=data,
+            auth=(username, password)
+        )
+
+        try:
+            json_resp = json.loads(r.content)
+        except ValueError:
+            json_resp = 'NoJSON'
+
+        if print_call:
+            header_string = ''
+            for key in headers:
+                header_string += '-H "%s: %s" ' % (key, headers[key])
+            print('\n*TESTCASE: %s, API Call: Create Folder*' % inspect.stack()[1][3])
+            print('\nCurl is:\n curl %s "%s" -d \'%s\' -u%s:%s -X %s' % (header_string, url, data, username, password,
+                                                                       method))
+            print('HTTP Code: %s' % r.status_code)
+            print('\nJSON response is:\n %s' % json_resp)
+
+        r.json = json_resp
+        return r
+
+    def delete_folder(self, folder_path, domain=None, method=None, content_type=None, accept=None,
+                      username=None, password=None, print_call=True):
+        if domain is None:
+            domain = self.config.domain
+        if method is None:
+            method = 'DELETE'
+        if content_type is None:
+            content_type = 'application/json'
+        if accept is None:
+            accept = 'application/json'
+        if username is None:
+            username = self.config.admin_login
+        if password is None:
+            password = self.config.password
+
+        endpoint = '/public-api/v1/fs'
+        url = '%s%s%s' % (domain, endpoint, folder_path)
+        headers = dict()
+        headers['Content-Type'] = content_type
+        headers['Accept'] = accept
+
+        r = requests.request(
+            method=method,
+            url=url,
+            headers=headers,
+            auth=(username, password)
+        )
+
+        try:
+            json_resp = json.loads(r.content)
+        except ValueError:
+            json_resp = 'NoJSON'
+
+        if print_call:
+            header_string = ''
+            for key in headers:
+                header_string += '-H "%s: %s" ' % (key, headers[key])
+            print('\n*TESTCASE: %s, API Call: Delete Folder*' % inspect.stack()[1][3])
+            print('\nCurl is:\n curl %s "%s" -u%s:%s -X %s' % (header_string, url, username, password, method))
+            print('HTTP Code: %s' % r.status_code)
+            print('\nJSON response is:\n %s' % json_resp)
+
+        r.json = json_resp
+        return r
+
+    def set_perms(self, folder_path, users, permission, domain=None, method=None, content_type=None, accept=None,
+                  username=None, password=None, print_call=True):
+        if domain is None:
+            domain = self.config.domain
+        if method is None:
+            method = 'POST'
+        if content_type is None:
+            content_type = 'application/json'
+        if accept is None:
+            accept = 'application/json'
+        if username is None:
+            username = self.config.admin_login
+        if password is None:
+            password = self.config.password
+
+        endpoint = '/public-api/v1/perms/folder'
+        url = '%s%s%s' % (domain, endpoint, folder_path)
+        headers = dict()
+        headers['Content-Type'] = content_type
+        headers['Accept'] = accept
+        data = dict()
+        data['users'] = []
+        data['permission'] = permission
+        if type(users) == list:
+            for user in users:
+                data['users'].append(user)
+        else:
+            data['users'].append(users)
+        data = json.dumps(data)
+
+        r = requests.request(
+            method=method,
+            url=url,
+            headers=headers,
+            data=data,
+            auth=(username, password)
+        )
+
+        try:
+            json_resp = json.loads(r.content)
+        except ValueError:
+            json_resp = 'NoJSON'
+
+        if print_call:
+            header_string = ''
+            for key in headers:
+                header_string += '-H "%s: %s" ' % (key, headers[key])
+            print('\n*TESTCASE: %s, API Call: Set Perms*' % inspect.stack()[1][3])
+            print('\nCurl is:\n curl %s "%s" -d \'%s\' -u%s:%s -X %s' % (header_string, url, data, username, password,
+                                                                         method))
+            print('HTTP Code: %s' % r.status_code)
+            print('\nJSON response is:\n %s' % json_resp)
+
+        r.json = json_resp
+        return r
+
+    def list_perms(self, folder_path, users, domain=None, method=None, content_type=None, accept=None,
+                   username=None, password=None, print_call=True):
+        if domain is None:
+            domain = self.config.domain
+        if method is None:
+            method = 'GET'
+        if content_type is None:
+            content_type = 'application/json'
+        if accept is None:
+            accept = 'application/json'
+        if username is None:
+            username = self.config.admin_login
+        if password is None:
+            password = self.config.password
+
+        endpoint = '/public-api/v1/perms/folder'
+        url = '%s%s%s' % (domain, endpoint, folder_path)
+        headers = dict()
+        headers['Content-Type'] = content_type
+        headers['Accept'] = accept
+
+        url += '?users=%s' % users
+
+        r = requests.request(
+            method=method,
+            url=url,
+            headers=headers,
+            auth=(username, password)
+        )
+
+        try:
+            json_resp = json.loads(r.content)
+        except ValueError:
+            json_resp = 'NoJSON'
+
+        if print_call:
+            header_string = ''
+            for key in headers:
+                header_string += '-H "%s: %s" ' % (key, headers[key])
+            print('\n*TESTCASE: %s, API Call: List Perms*' % inspect.stack()[1][3])
+            print('\nCurl is:\n curl %s "%s" -u%s:%s -X %s' % (header_string, url, username, password, method))
+            print('HTTP Code: %s' % r.status_code)
+            print('\nJSON response is:\n %s' % json_resp)
+
+        r.json = json_resp
+        return r
 
 
-    r = requests.request(
-        method=method,
-        url=url,
-        headers=headers,
-        auth=(username, password)
-    )
+class Utils:
+    def __init__(self):
+        self.config = Config()
 
-    try:
-        json_resp = json.loads(r.content)
-    except ValueError:
-        json_resp = 'NoJSON'
-
-    r.json = json_resp
-    return r
+    @staticmethod
+    def random_name():
+        return 'test_name%s' % randint(1000000, 9999999)
--- api_framework/tests/pubapi/testPubCreateFolder.py
+++ api_framework/tests/pubapi/testPubCreateFolder.py
@@ -1,21 +1,43 @@
-import pubapiutils
+from pubapiutils import Calls
+from pubapiutils import Config
+from pubapiutils import Utils
 import httplib
 
 
-def test_create_100_folders_positive():
-    no_json = 'NoJSON'
-    for i in range(5):
-        folder_name = 'test_folder123%s' % i
-        resp = pubapiutils.create_folder(folder_name)
-        assert resp.status_code == httplib.CREATED
-        assert resp.json == no_json
-        pubapiutils.del_folder(folder_name)
+class TestClass:
+    def __init__(self):
+        self.no_json = 'NoJSON'
+        self.calls = Calls()
+        self.config = Config()
+        self.utils = Utils()
 
-def test_create_folders_positive():
-    no_json = 'NoJSON'
-    for i in range(3):
-        folder_name = 'test_folder123gtg%s' % i
-        resp = pubapiutils.create_folder(folder_name)
-        assert resp.status_code == httplib.CREATED
-        assert resp.json == no_json
-        pubapiutils.del_folder(folder_name)
+    def test_create_5_folders_positive(self):
+        l = []
+        for i in range(5):
+            folder_name = self.utils.random_name()
+            resp = self.calls.create_folder(folder_name)
+            assert resp.status_code == httplib.CREATED
+            assert resp.json == self.no_json
+            l.append(folder_name)
+        for item in l:
+            resp = self.calls.delete_folder(folder_path=self.config.testpath + item)
+            assert resp.status_code == httplib.OK
+
+    def test_create_folder_enough_perms(self):
+        folder1 = self.utils.random_name()
+        folder2 = self.utils.random_name()
+        perms = ['Editor', 'Full', 'Owner']
+        for perm in perms:
+            folder_path = '%s/%s' % (self.config.testpath, folder1)
+            self.calls.create_folder(folder1)
+            resp = self.calls.set_perms(folder_path=folder_path, users=self.config.puser, permission=perm)
+            assert resp.status_code == httplib.OK
+            resp = self.calls.list_perms(folder_path=folder_path, users=self.config.puser)
+            assert resp.status_code == httplib.OK
+            assert resp.json['users'][0]['permission'] == perm
+            assert resp.json['users'][0]['subject'] == self.config.puser
+            assert len(resp.json['groups']) == 0
+            resp = self.calls.create_folder(folder2, path=folder_path, username=self.config.puser)
+            assert resp.status_code == httplib.CREATED
+            assert resp.json == self.no_json
+            self.calls.delete_folder(folder_path)
